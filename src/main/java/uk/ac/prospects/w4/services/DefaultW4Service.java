8a94923c62032d486cf4ad31f18e733e8db52733
vasileiosl <v.liolios@prospects.ac.uk>
2012-11-16 14:16:55 +0000

    Added a dummy service class to make git commit the folders

---
+++ src/main/java/uk/ac/prospects/w4/services/DefaultW4Service.java
@@ -0,0 +1,10 @@
+package uk.ac.prospects.w4.services;
+
+/**
+ * @author vasileiosl
+ *         Date: 16/11/12
+ *         Time: 14:14
+ */
+public class DefaultW4Service {
+	//Dummy file for git to add the folder
+}
