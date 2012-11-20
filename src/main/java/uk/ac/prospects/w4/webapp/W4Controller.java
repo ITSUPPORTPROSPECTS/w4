9f575f525ea60e7c96f9de3ce73347c3a3054ba2
shuai geng <s.geng@prospects.ac.uk>
2012-11-20 23:52:55 +0000

    wwww-1: add test method for w4 controller

--- src/main/java/uk/ac/prospects/w4/webapp/W4Controller.java
+++ src/main/java/uk/ac/prospects/w4/webapp/W4Controller.java
@@ -5,6 +5,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.xml.sax.SAXException;
+import uk.ac.prospects.w4.domain.Course;
+import uk.ac.prospects.w4.repository.CourseGenerator;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * @author vasileiosl
@@ -14,12 +22,20 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class W4Controller {
 
-	@RequestMapping(value="/{page}.htm", method = RequestMethod.GET)
-	public ModelAndView anyPage(@PathVariable String page){
+	@RequestMapping(value = "/{page}.htm", method = RequestMethod.GET)
+	public ModelAndView anyPage(@PathVariable String page) {
 
 		ModelAndView model = new ModelAndView(page);
 		model.addObject("msg", "Any page");
 		return model;
+	}
+
+	@RequestMapping(value = "/test.htm", method = RequestMethod.GET)
+	public void retrieveCourseProviderAndCourse() throws IOException, SAXException, XPathExpressionException, ParserConfigurationException {
+		//ModelAndView model = new ModelAndView(page);
+		List<Course> courses = CourseGenerator.generateCoursesFromJsonSearchResult("");
+		System.out.println(courses.size());
+		//return model;
 	}
 
 }
