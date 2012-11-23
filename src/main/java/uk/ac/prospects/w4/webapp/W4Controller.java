695760e02badfdbbf826ba34d03cce0afde5a356
Malvine Logina <m.logina@gmail.com>
2012-11-23 14:02:21 +0000

    .

--- src/main/java/uk/ac/prospects/w4/webapp/W4Controller.java
+++ src/main/java/uk/ac/prospects/w4/webapp/W4Controller.java
@@ -1,6 +1,7 @@
 package uk.ac.prospects.w4.webapp;
 
 import org.apache.commons.lang.BooleanUtils;
+import org.apache.commons.lang.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +18,11 @@ import uk.ac.prospects.w4.services.helper.CourseGenerator;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathExpressionException;
 import java.io.IOException;
+import java.io.Serializable;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -82,4 +87,206 @@ public class W4Controller {
 		model.setViewName("map");
 		return model;
 	}
+
+
+
+
+@RequestMapping(value = "/calendar.htm", method = RequestMethod.GET)
+	public ModelAndView retriveCalendar(@RequestParam(value = "provid", required = false) String provId,
+															@RequestParam(value = "fromStartDate", required = false) String fromStartDate,
+															@RequestParam(value = "toStartDate", required = false) String toStartDate,
+															@RequestParam(value = "keyword", required = false) String keyword,
+															@RequestParam(value = "startDate", required = false) String startDate,
+															@RequestParam(value = "courseTitle", required = false) String courseTitle,
+															@RequestParam(value = "day", required=false) Integer day,
+															@RequestParam(value = "month", required=false) Integer month,
+															@RequestParam(value = "year", required=false) Integer year) throws IOException, SAXException, XPathExpressionException, ParseException, ParserConfigurationException {
+
+
+		final String [] MONTHS =
+		new String [] {"January", "February", "March", "April", "May",
+		"June", "July", "August", "September", "October", "November", "December"};
+
+
+	if(day==null || month== null || year==null){
+		Calendar todaysCalendar = Calendar.getInstance();
+		todaysCalendar.setTime(new Date());
+		day=todaysCalendar.get(Calendar.DAY_OF_MONTH);
+		month=todaysCalendar.get(Calendar.MONTH) + 1;
+		year = todaysCalendar.get(Calendar.YEAR);
+	}
+
+	Calendar calendar = Calendar.getInstance();
+	calendar.set(year,month-1, day);
+	Calendar monthCalendar = Calendar.getInstance();
+	monthCalendar.set(year,month-1, day);
+	monthCalendar.set(Calendar.DAY_OF_MONTH, monthCalendar.getActualMinimum(Calendar.DAY_OF_MONTH) );
+	SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+	fromStartDate = format.format(monthCalendar.getTime());
+	System.out.println("fromStartDate="+ fromStartDate);
+
+	monthCalendar.set(Calendar.DAY_OF_MONTH, monthCalendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+	toStartDate = format.format(monthCalendar.getTime());
+	System.out.println("toStartDate="+ toStartDate);
+
+
+
+
+
+
+/*
+	if(fromStartDate ==null || fromStartDate.isEmpty() ){
+		Calendar monthCalendar = Calendar.getInstance();
+		monthCalendar.setTime(new Date());
+		monthCalendar.set(Calendar.DAY_OF_MONTH, monthCalendar.getActualMinimum(Calendar.DAY_OF_MONTH) );
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		fromStartDate = format.format(monthCalendar.getTime());
+		System.out.println("fromStartDate="+ fromStartDate);
+
+		monthCalendar.set(Calendar.DAY_OF_MONTH, monthCalendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+		toStartDate = format.format(monthCalendar.getTime());
+		System.out.println("toStartDate="+ toStartDate);
+
+		
+	}
+*/
+
+		ModelAndView model = retrieveCourseProviderAndCourse(provId,fromStartDate, toStartDate, keyword, startDate,courseTitle, false);
+		model.setViewName("calendar");
+		/*Calendar todaysCalendar = Calendar.getInstance();
+			todaysCalendar.setTime(new Date());
+		*/
+		CalendarValues selectedCalendar = new CalendarValues(calendar);
+
+		model.addObject("selectedCalendar", selectedCalendar);
+		/*model.addObject("selectedMonthTitle", MONTHS[calendar.get(Calendar.MONTH)]);
+		model.addObject("selectedYear", calendar.get(Calendar.YEAR));
+		model.addObject("selectedDay", calendar.get(Calendar.DAY_OF_MONTH));
+		model.addObject("selectedMonth", calendar.get(Calendar.MONTH)+1);
+*/
+
+
+		Calendar todays = Calendar.getInstance();
+		todays.setTime(new Date());
+
+		CalendarValues todaysCalendar = new CalendarValues(todays);
+		model.addObject("todaysCalendar", todaysCalendar);
+
+		/*model.addObject("todaysMonth", todaysCalendar.get(Calendar.MONTH) + 1);
+		model.addObject("todaysDay", todaysCalendar.get(Calendar.DAY_OF_MONTH));
+		model.addObject("todaysYear", todaysCalendar.get(Calendar.YEAR));
+*/
+		
+		//create month calendar
+		monthCalendar = Calendar.getInstance();
+		monthCalendar.set(year, month - 1, day);
+		int daysInMonth = monthCalendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+		model.addObject("daysInMonth", daysInMonth);
+
+		Calendar firstDayOfTheMonth = DateUtils.truncate(monthCalendar, Calendar.MONTH);
+
+		model.addObject("firstDayOfTheMonth", firstDayOfTheMonth.get(Calendar.DAY_OF_MONTH));
+		int firstDayOfTheMonthWeekday = firstDayOfTheMonth.get(Calendar.DAY_OF_WEEK);
+		if(firstDayOfTheMonthWeekday==1){
+			firstDayOfTheMonthWeekday = 7;
+		}else {
+			firstDayOfTheMonthWeekday--;
+		}
+		model.addObject("firstDayOfTheMonthWeekday", firstDayOfTheMonthWeekday);
+
+
+		List<Course> courses = (List<Course>) model.getModel().get("courses");
+
+		//max days in a month
+		int[] dates = new int[32];
+		for(Course course : courses){
+			Calendar courseCalendar = Calendar.getInstance();
+			courseCalendar.setTime(course.getStartDate());
+			int dayNumber = courseCalendar.get(Calendar.DAY_OF_MONTH);
+			dates[dayNumber] = 1;
+		}
+
+
+		fromStartDate = format.format(calendar.getTime());
+		System.out.println("fromStartDateForDay="+ fromStartDate);
+		toStartDate = fromStartDate;
+		ModelAndView modelForSelectedDay = retrieveCourseProviderAndCourse(provId,fromStartDate, toStartDate, keyword, startDate,courseTitle, false);
+		List<Course> coursesForSelectedDay = (List<Course>) modelForSelectedDay.getModel().get("courses");
+
+		model.addObject("courses", coursesForSelectedDay);
+		model.addObject("dates", dates);
+
+		Calendar previousMonthCalendar = Calendar.getInstance();
+		previousMonthCalendar.setTime(calendar.getTime());
+		previousMonthCalendar.add(Calendar.MONTH, -1);
+
+		model.addObject("previousMonth", previousMonthCalendar.get(Calendar.MONTH)+1);
+		model.addObject("previousMonthYear", previousMonthCalendar.get(Calendar.YEAR));
+		model.addObject("previousMonthLastDay", previousMonthCalendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+		model.addObject("previousMonthTitle", MONTHS[previousMonthCalendar.get(Calendar.MONTH)]);
+
+		Calendar nextMonthCalendar = Calendar.getInstance();
+		nextMonthCalendar.setTime(calendar.getTime());
+		nextMonthCalendar.add(Calendar.MONTH, 1);
+		model.addObject("nextMonth", nextMonthCalendar.get(Calendar.MONTH)+1);
+		model.addObject("nextMonthYear", nextMonthCalendar.get(Calendar.YEAR));
+		model.addObject("nextMonthFirstDay",nextMonthCalendar.getActualMinimum(Calendar.DAY_OF_MONTH));
+		model.addObject("nextMonthTitle", MONTHS[nextMonthCalendar.get(Calendar.MONTH)]);
+
+
+
+
+
+		return model;
+	}
+
+	public class CalendarValues implements Serializable{
+		private static final long serialVersionUID = -7723338363691165352L;
+		private int month;
+		private int year;
+		private int day;
+		private int monthLastDay;
+		private int monthFirstDay;
+		private String monthTitle;
+
+		final String [] MONTHS =
+		new String [] {"January", "February", "March", "April", "May",
+		"June", "July", "August", "September", "October", "November", "December"};
+
+		public int getMonth() {
+			return month;
+		}
+
+		public int getYear() {
+			return year;
+		}
+
+		public int getMonthLastDay() {
+			return monthLastDay;
+		}
+
+		public int getMonthFirstDay() {
+			return monthFirstDay;
+		}
+
+		public String getMonthTitle() {
+			return monthTitle;
+		}
+
+		public int getDay() {
+			return day;
+		}
+
+		CalendarValues(Calendar calendar){
+			this.month = calendar.get(Calendar.MONTH)+1;
+			this.year = calendar.get(Calendar.YEAR);
+			this.day = calendar.get(Calendar.DAY_OF_MONTH);
+			this.monthLastDay = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+			this.monthFirstDay = calendar.getActualMinimum(Calendar.DAY_OF_MONTH);
+			this.monthTitle = MONTHS[calendar.get(Calendar.MONTH)];
+		}
+		
+	}
+
 }
--- src/main/webapp/WEB-INF/pages/calendar.jsp
+++ src/main/webapp/WEB-INF/pages/calendar.jsp
@@ -1,13 +1,15 @@
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@include file="inc/header_inc.jsp" %>
 
 	<div class="calendar_nav">
-		<a href="/pages/prospects/myprospects_dashboard.htm?_eventId=previousMonth&amp;month=11&amp;year=2012" title="Events in October 2012" class="prev_month prev plainlink">&nbsp;<span class="offscreen">Events in October 2012</span></a>
+		<a href="/calendar.htm?month=${previousMonth}&amp;year=${previousMonthYear}&amp;day=${previousMonthLastDay}" title="Events in October 2012" class="prev_month prev plainlink">&nbsp;<span class="offscreen">Events in October 2012</span></a>
 		<h2>
-			November 2012
-		</h2><a href="/pages/prospects/myprospects_dashboard.htm?_eventId=nextMonth&amp;month=11&amp;year=2012" title="Events in December 2012" class="next_month next plainlink">&nbsp;<span class="offscreen">Events in December 2012</span></a>
+			<c:out value="${selectedCalendar.monthTitle}"/> ${selectedCalendar.year}
+		</h2><a href="/calendar.htm?month=${nextMonth}&amp;year=${nextMonthYear}&amp;day=${nextMonthFirstDay}" title="Events in December 2012" class="next_month next plainlink">&nbsp;<span class="offscreen">Events in December 2012</span></a>
 	</div>
-	<table summary="Contains links to events in November 2012" class="calendar">
+	<table summary="Contains links to events in ${selectedCalendar.monthTitle} ${selectedCalendar.year}" class="calendar">
 		<thead>
 			<tr>
 				<th>
@@ -34,24 +36,111 @@
 			</tr>
 		</thead>
 		<tbody>
-			<tr class="first_week">
-				<td class="prev_month past_date"><span>&nbsp;</span></td>
-				<td class="prev_month past_date"><span>&nbsp;</span></td>
-				<td class="prev_month past_date"><span>&nbsp;</span></td>
-				<td class="past_date">
-					<span>1</span>
-				</td>
-				<td class="past_date">
-					<a href="/pages/prospects/myprospects_dashboard.htm?_eventId=viewDay&amp;month=11&amp;year=2012&amp;day=2" title="Events on 2 November 2012">2</a>
-				</td>
-				<td class="weekend past_date">
-					<span>3</span>
-				</td>
-				<td class="weekend past_date">
-					<span>4</span>
-				</td>
-			</tr>
-			<tr>
+
+		<c:set var="loop_date" value="1"/>
+		<c:set var="firstDaySet" value="false"/>
+
+		<c:forEach var="week_number" begin="0" end="5" step="1" varStatus ="status">
+
+			<c:if test="${loop_date <= daysInMonth}">
+			<c:set var="first_week_class" value=""/>
+				<c:if test="${week_number==0}">
+					<c:set var="first_week_class" value="first_week"/>
+				</c:if>
+
+
+				<tr class="${first_week_class}">
+					<c:forEach var="weekday" begin="1" end="7" step="1" varStatus ="status">
+						<c:choose>
+							<c:when test="${firstDaySet}">
+
+
+								<c:set var="cssClass" value=""/>
+								<c:choose>
+									<c:when test="${loop_date <= daysInMonth}">
+											<c:if test="${loop_date==todaysCalendar.day && selectedCalendar.month==todaysCalendar.month && selectedCalendar.year== todaysCalendar.year}">
+												<c:set var="cssClass" value="${cssClass} today"/>
+											</c:if>
+											<c:if test="${selectedCalendar.year<todaysCalendar.year && selectedCalendar.month< todaysCalendar.month && loop_date<todaysCalendar.day}">
+												<c:set var="cssClass" value="${cssClass} past_date"/>
+											</c:if>
+											<c:if test="${weekday>=6}">
+												<c:set var="cssClass" value="${cssClass} weekend"/>
+											</c:if>
+											<c:if test="${loop_date==selectedCalendar.day}">
+												<c:set var="cssClass" value="${cssClass} selected_day"/>
+											</c:if>
+										<td class="${cssClass}">
+													<span>
+													<c:choose>
+														<c:when test="${dates[loop_date]==1}">
+															<a href="/calendar.htm?day=${loop_date}&amp;month=${selectedCalendar.month}&amp;year=${selectedCalendar.year}" >${loop_date}</a>
+														</c:when>
+														<c:otherwise>
+															${loop_date}
+														</c:otherwise>
+													</c:choose>
+
+													</span>
+												</td>
+									<c:set var="loop_date" value="${loop_date+1}"/>
+									</c:when>
+									<c:otherwise>
+										<c:set var="cssClass" value="${cssClass} next_month"/>
+										<c:if test="${weekend >= 6}">
+											<c:set var="cssClass" value="${cssClass} weekend"/>
+										</c:if>
+
+
+										<td class="${cssClass}"><span>&nbsp;</span></td>
+									</c:otherwise>
+								</c:choose>
+
+							</c:when>
+							<c:otherwise>
+								<c:choose>
+									<c:when test="${firstDayOfTheMonthWeekday != weekday}">
+										<td class="prev_month past_date"><span>&nbsp;</span></td>
+									</c:when>
+									<c:when test="${firstDayOfTheMonthWeekday==weekday}">
+									<c:set var="cssClass" value=""/>
+											<c:if test="${firstDayOfTheMonth==todaysCalendar.day && selectedCalendar.month==todaysCalendar.month && selectedCalendar.year == todaysCalendar.year}">
+												<c:set var="cssClass" value="${cssClass} today"/>
+											</c:if>
+											<c:if test="${selectedCalendar.year<todaysCalendar.year && selectedCalendar.month<todaysCalendar.month && firstDayOfTheMonth<todaysCalendar.day}">
+												<c:set var="cssClass" value="${cssClass} past_date"/>
+											</c:if>
+											<c:if test="${weekday>6}">
+												<c:set var="cssClass" value="${cssClass} weekend"/>
+											</c:if>
+											<c:if test="${firstDayOfTheMonth==selectedCalendar.day}">
+												<c:set var="cssClass" value="${cssClass} selected_day"/>
+											</c:if>
+										<td class="${cssClass}">
+													<span>
+														<c:choose>
+														<c:when test="${dates[loop_date]==1}">
+															<a href="/calendar.htm?day=${loop_date}&amp;month=${selectedCalendar.month}&amp;year=${selectedCalendar.year}" >${loop_date}</a>
+														</c:when>
+														<c:otherwise>
+															${loop_date}
+														</c:otherwise>
+													</c:choose>
+													</span>
+												</td>
+										<c:set var="loop_date" value="${loop_date+1}"/>
+										<c:set var="firstDaySet" value="true"/>
+									</c:when>
+								</c:choose>
+							</c:otherwise>
+						</c:choose>
+					</c:forEach>
+					</tr>
+
+			</c:if>
+			</c:forEach>
+
+		<%--	<tr>
 				<td class="past_date">
 					<a href="/pages/prospects/myprospects_dashboard.htm?_eventId=viewDay&amp;month=11&amp;year=2012&amp;day=5" title="Events on 5 November 2012">5</a>
 				</td>
@@ -119,8 +208,8 @@
 				<td class="weekend">
 					<span>25</span>
 				</td>
-			</tr>
-			<tr>
+			</tr>--%>
+			<%--<tr>
 				<td class="">
 					<a href="/pages/prospects/myprospects_dashboard.htm?_eventId=viewDay&amp;month=11&amp;year=2012&amp;day=26" title="Events on 26 November 2012">26</a>
 				</td>
@@ -138,8 +227,11 @@
 				</td>
 				<td class="next_month weekend"><span>&nbsp;</span></td>
 				<td class="next_month weekend"><span>&nbsp;</span></td>
-			</tr>
+			</tr>--%>
 		</tbody>
 	</table>
+
+
+<%@include file="index.jsp"%>
 
 <%@include file="inc/footer_inc.jsp" %>
