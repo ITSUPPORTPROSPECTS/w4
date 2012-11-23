fa836971490ccea1cc4236b0908158a60b2d8ff6
vasileiosl <v.liolios@prospects.ac.uk>
2012-11-23 16:29:17 +0000

    WWWW-22: Added the last tests for UrlBuilder

--- src/test/java/uk/ac/prospects/w4/webapp/UrlBuilderTest.java
+++ src/test/java/uk/ac/prospects/w4/webapp/UrlBuilderTest.java
@@ -33,7 +33,7 @@ public class UrlBuilderTest {
 
 	@Test
 	public void testBuildCalendarURLForCalendarPage(){
-		String paramString = UrlBuilder.buildCalendarURLForCalendarPage("id", "provider title", "a keyword",  "course title", "preserve=fromStartDate:2011-09-01,toStartDate:2013-01-01");
+		String paramString = UrlBuilder.buildCalendarURLForCalendarPage("id", "provider title", "a keyword",  "course title", "fromStartDate:2011-09-01,toStartDate:2013-01-01");
 
 		assertThat(paramString.contains("provid=id"), CoreMatchers.equalTo(true));
 		assertThat(paramString.contains("provTitle=provider title"), CoreMatchers.equalTo(true));
@@ -47,7 +47,7 @@ public class UrlBuilderTest {
 
 	@Test
 	public void testBuildGeneralURLForCalendarPage(){
-		String paramString = UrlBuilder.buildGeneralURLForCalendarPage("id", "provider title", "a keyword",  "course title", "preserve=fromStartDate:2011-09-01,toStartDate:2013-01-01");
+		String paramString = UrlBuilder.buildGeneralURLForCalendarPage("id", "provider title", "a keyword",  "course title", "fromStartDate:2011-09-01,toStartDate:2013-01-01");
 
 		assertThat(paramString.contains("provid=id"), CoreMatchers.equalTo(true));
 		assertThat(paramString.contains("provTitle=provider title"), CoreMatchers.equalTo(true));
@@ -81,5 +81,51 @@ public class UrlBuilderTest {
 	public void testBuildGeneralURLForCalendarPageNullValues(){
 		String paramString = UrlBuilder.buildGeneralURLForCalendarPage(null, null, null, null, null);
 		assertThat(paramString, CoreMatchers.equalTo(""));
+	}
+
+	@Test
+	public void testBuildCalendarURLOnlyStartDates(){
+		String paramString = UrlBuilder.buildCalendarURL(null, null, null, "2011-09-01", "2013-01-01", null);
+
+		assertThat(paramString.contains("provid"), CoreMatchers.equalTo(false));
+		assertThat(paramString.contains("provTitle"), CoreMatchers.equalTo(false));
+		assertThat(paramString.contains("keyword"), CoreMatchers.equalTo(false));
+		assertThat(paramString.contains("courseTitle"), CoreMatchers.equalTo(false));
+		assertThat(paramString.contains("preserve=fromStartDate:2011-09-01,toStartDate:2013-01-01")
+						|| paramString.contains("preserve=toStartDate:2013-01-01,fromStartDate:2011-09-01"), CoreMatchers.equalTo(true));
+	}
+
+	@Test
+	public void testBuildGeneralURLOnlyStartDates(){
+		String paramString = UrlBuilder.buildGeneralURL(null, null, null, "2011-09-01", "2013-01-01", null);
+
+		assertThat(paramString.contains("provid"), CoreMatchers.equalTo(false));
+		assertThat(paramString.contains("provTitle"), CoreMatchers.equalTo(false));
+		assertThat(paramString.contains("keyword"), CoreMatchers.equalTo(false));
+		assertThat(paramString.contains("courseTitle"), CoreMatchers.equalTo(false));
+		assertThat(paramString.contains("fromStartDate=2011-09-01"), CoreMatchers.equalTo(true));
+		assertThat(paramString.contains("toStartDate=2013-01-01"), CoreMatchers.equalTo(true));
+	}
+
+	@Test
+	public void testBuildCalendarURLForCalendarPageOnlyStartDates(){
+		String paramString = UrlBuilder.buildCalendarURLForCalendarPage(null, null, null, null, "fromStartDate:2011-09-01,toStartDate:2013-01-01");
+
+		assertThat(paramString.equals("preserve=fromStartDate:2011-09-01,toStartDate:2013-01-01")
+						|| paramString.equals("preserve=toStartDate:2013-01-01,fromStartDate:2011-09-01"), CoreMatchers.equalTo(true));
+	}
+
+	@Test
+	public void testBuildGeneralURLForCalendarPageOnlyStartDates(){
+		String paramString = UrlBuilder.buildGeneralURLForCalendarPage(null, null, null, null, "fromStartDate:2011-09-01,toStartDate:2013-01-01");
+
+		assertThat(paramString.contains("provid"), CoreMatchers.equalTo(false));
+		assertThat(paramString.contains("provTitle"), CoreMatchers.equalTo(false));
+		assertThat(paramString.contains("keyword"), CoreMatchers.equalTo(false));
+		assertThat(paramString.contains("courseTitle"), CoreMatchers.equalTo(false));
+		assertThat(paramString.contains("fromStartDate=2011-09-01"), CoreMatchers.equalTo(true));
+		assertThat(paramString.contains("toStartDate=2013-01-01"), CoreMatchers.equalTo(true));
+		assertThat(paramString.contains("preserve=fromStartDate:2011-09-01,toStartDate:2013-01-01")
+						&& paramString.contains("preserve=toStartDate:2013-01-01,fromStartDate:2011-09-01"), CoreMatchers.equalTo(false));
 	}
 }
